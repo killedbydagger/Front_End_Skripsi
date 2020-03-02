26fd4914330cb36ed1e8d63deaf7381cf40a9edf
lievincent <vincentxxxlie@yahoo.com>
2020-03-02 16:39:30 +0700

    sign up, halam login

--- app/src/main/java/com/example/skripsi/LoginActivity.java
+++ app/src/main/java/com/example/skripsi/LoginActivity.java
@@ -1,13 +1,28 @@
 package com.example.skripsi;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    TextView tvSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+        tvSignup = findViewById(R.id.tv_signup);
+        tvSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent singupIntent = new Intent(getApplicationContext(),SignupActivity.class);
+                startActivity(singupIntent);
+            }
+        });
     }
 }
