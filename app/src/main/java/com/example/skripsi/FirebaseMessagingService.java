90c404c9a62c6defbd8b16890c1559f021032ad9
lievincent <vincentxxxlie@yahoo.com>
2020-06-06 21:08:18 +0700

    vin

--- app/src/main/java/com/example/skripsi/FirebaseMessagingService.java
+++ app/src/main/java/com/example/skripsi/FirebaseMessagingService.java
@@ -56,12 +56,12 @@ public class FirebaseMessagingService extends com.google.firebase.messaging.Fire
             //builder=builder.setContent(getCustomeDesign(title,message));
             builder=builder.setContentTitle(title)
                     .setContentText(message)
-                    .setSmallIcon(R.drawable.ic_stat_name).setColor(getResources().getColor(R.color.colorBlueJeans));
+                    .setSmallIcon(R.drawable.ic_stat_name).setColor(getResources().getColor(R.color.colorMint));
         }
         else{
             builder=builder.setContentTitle(title)
                     .setContentText(message)
-                    .setSmallIcon(R.drawable.ic_stat_name).setColor(getResources().getColor(R.color.colorBlueJeans));
+                    .setSmallIcon(R.drawable.ic_stat_name).setColor(getResources().getColor(R.color.colorMint));
         }
 
         NotificationManager notificationManager= (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
